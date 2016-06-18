c918e9b049e97c16769d879654f685c8cc7a65e7
zecarlos94 <zecarlos_94@hotmail.com>
2016-06-18 19:04:59 +0100

    mapa percurso
    Mapa de percurso quase pronto

---
+++ Web/Interrail/Interrail/RoutesMap.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RoutesMap.aspx.cs" Inherits="Interrail.RoutesMap" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+     <link href="styles/RoutesMapStyle.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="scripts/saveImageMaps.js"></script>
+    <table border="0" cellpadding="0" cellspacing="0">
+        <tr>
+            <td>
+                <div id="dvMap" style="width: 250px; height: 350px">
+                </div>
+            </td>
+            <td>
+                &nbsp; &nbsp;
+            </td>
+            <td>
+                <img id="imgMap" alt="" style="display: none" />
+            </td>
+        </tr>
+    </table>
+    <br />
+    <input type="button" id="btnExport" value="Export" onclick="SaveImg()" />
+</body>
+</html>
---
+++ Web/Interrail/Interrail/scripts/saveImageMaps.js
@@ -0,0 +1,112 @@
+﻿var markers = [
+           {
+               "title": 'Real',
+               "lat": '41.553065',
+               "lng": '-8.435386',
+               "description": 'Real is the best.'
+           }
+       ,
+           {
+               "title": 'Uminho',
+               "lat": '41.558423',
+               "lng": '-8.397457',
+               "description": 'Uminho is nice.'
+           }
+];
+
+window.onload = function () {
+    LoadMap();
+}
+var map, mapOptions;
+function LoadMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(markers[0].lat, markers[0].lng),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("dvMap"), mapOptions);
+    var infoWindow = new google.maps.InfoWindow();
+    var lat_lng = new Array();
+    var latlngbounds = new google.maps.LatLngBounds();
+    for (i = 0; i < markers.length; i++) {
+        var data = markers[i]
+        var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+        lat_lng.push(myLatlng);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: data.title
+        });
+        latlngbounds.extend(marker.position);
+        (function (marker, data) {
+            google.maps.event.addListener(marker, "click", function (e) {
+                infoWindow.setContent(data.description);
+                infoWindow.open(map, marker);
+            });
+        })(marker, data);
+    }
+    map.setCenter(latlngbounds.getCenter());
+    map.fitBounds(latlngbounds);
+
+    //***********ROUTING****************//
+
+    //Intialize the Path Array
+    var path = new google.maps.MVCArray();
+
+    //Intialize the Direction Service
+    var service = new google.maps.DirectionsService();
+
+    //Set the Path Stroke Color
+    var poly = new google.maps.Polyline({ map: map, strokeColor: '#4986E7' });
+
+    //Loop and Draw Path Route between the Points on MAP
+    for (var i = 0; i < lat_lng.length; i++) {
+        if ((i + 1) < lat_lng.length) {
+            var src = lat_lng[i];
+            var des = lat_lng[i + 1];
+            path.push(src);
+            poly.setPath(path);
+            service.route({
+                origin: src,
+                destination: des,
+                travelMode: google.maps.DirectionsTravelMode.DRIVING
+            }, function (result, status) {
+                if (status == google.maps.DirectionsStatus.OK) {
+                    for (var i = 0, len = result.routes[0].overview_path.length; i < len; i++) {
+                        path.push(result.routes[0].overview_path[i]);
+                    }
+                }
+            });
+        }
+    }   
+};
+
+function SaveImg() {
+    //URL of Google Static Maps.
+    //Full Example, 
+    // var staticMapUrl = "http://maps.googleapis.com/maps/api/staticmap?size=250x350&path=40.737102,-73.990318|40.749825,-73.987963|40.752946,-73.987384|40.755823,-73.986397&sensor=false ";
+   
+    var staticMapUrl = "http://maps.googleapis.com/maps/api/staticmap?size=250x350";
+     
+    //Loop and add Markers.
+    for (var i = 0; i < markers.length; i++) {
+        staticMapUrl += "&markers=color:red|" + markers[i].lat + "," + markers[i].lng;
+    }
+
+    //Set first markers path
+    staticMapUrl += "&path=" + markers[0].lat + "," + markers[0].lng;
+
+    //Loop and add rest of Markers.
+    for (var i = 1; i < markers.length; i++) {
+        staticMapUrl += "|" + markers[i].lat + "," + markers[i].lng;
+    }
+
+    //Set sensor
+    staticMapUrl += "&sensor=false ";
+    
+    //Display the Image of Google Map.
+    var imgMap = document.getElementById("imgMap");
+    imgMap.src = staticMapUrl;
+    imgMap.style.display = "block";
+}
+ 
