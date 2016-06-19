2020cd1e2bd10514d8953ee64add8827a87bb8c2
zangetsou <andre.scb.pinto@gmail.com>
2016-06-19 05:29:50 +0100

    Alteração InserirLocais.sql e leitura da informação dos locais da agenda
    Falta inserir informação nos markers. Possível alteração da
    representação das coordenadas na BD

--- Web/Interrail/Interrail/RoutesMap.aspx.cs
+++ Web/Interrail/Interrail/RoutesMap.aspx.cs
@@ -4,14 +4,61 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;
+using System.Data.SqlClient;
+using Interrail.classes;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 namespace Interrail
 {
     public partial class RoutesMap : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string email, agenda;
+            SqlCommand cmmmd;
+            SqlDataReader tasks;
+            SqlConnection connn = new SqlConnection(@"Data Source=TIAGO-PC\TIAGOSERVER;Initial Catalog=Interrail;Integrated Security=True");
 
+            email = Request.QueryString["id"];
+            agenda = Request.QueryString["agenda"];
+            cmmmd = new SqlCommand("SELECT Local FROM Tarefa WHERE fk_Agenda = @Id", connn);
+            cmmmd.Parameters.AddWithValue("@Id", agenda);
+
+            connn.Open();
+
+            tasks = cmmmd.ExecuteReader();
+
+            while (tasks.Read())
+            {
+
+                SqlCommand cmd;
+                SqlDataReader locais;
+                SqlConnection con = new SqlConnection(@"Data Source=TIAGO-PC\TIAGOSERVER;Initial Catalog=Interrail;Integrated Security=True");
+                
+                cmd = new SqlCommand("SELECT Descricao, Coordenadas.ToString(), Cidade, Pais FROM Local WHERE Id = @Id", con);
+                cmd.Parameters.AddWithValue("@Id", tasks.GetValue(0));
+
+                con.Open();
+
+                locais = cmd.ExecuteReader();
+                string title = locais.GetString(0);
+                string description = locais.GetString(2) + "," + locais.GetString(3);
+                string coords = locais.GetString(1);
+                string[] stringSeparators = new string[] { " " };
+                string[] latLong = coords.Split(stringSeparators, StringSplitOptions.None);
+                string lat = ((string)latLong[0]).Substring(7, latLong.Length - 7);
+                string lon = ((string)latLong[1]).Substring(0, latLong.Length-1);
+                double latitude = Convert.ToDouble(lat);
+                double longitude = Convert.ToDouble(lon);
+            }
         }
     }
 }
--- Web/Scripts/Inserir Locais.sql
+++ Web/Scripts/Inserir Locais.sql
@@ -9,3 +9,21 @@ VALUES (1, 'Torre Eiffel', geography::STGeomFromText('LINESTRING(48 51.30, 2 17.
 	   (6, 'Siegestor', geography::STGeomFromText('LINESTRING(48 09.08, 11 34.55)', 4326), 'Munich', 'Germany'),
 	   (7, 'Coleseum', geography::STGeomFromText('LINESTRING(41 53.24, 12 29.32)', 4326), 'Rome', 'Italy');
 
+
+
+
+select Id,  
+       Coordenadas.ToString() as Point
+from Local
+
+
+
+INSERT INTO Local (Id, Descricao, Coordenadas, Cidade, Pais)
+VALUES (1, 'Torre Eiffel', geography::STGeomFromText('POINT(51.30 17.40)', 4326), 'Paris', 'France'),
+	   (2, 'National Monument', geography::STGeomFromText('POINT(22.22 53.37)', 4326), 'Amsterdam', 'The Netherlands'),
+	   (3, 'Anne Frank House', geography::STGeomFromText('POINT( 0.0  0.0)', 4326), 'Amsterdam', 'The Netherlands'),
+	   (4, 'The Motherland Calls', geography::STGeomFromText('POINT(44.33 32.13)', 4326), 'Volgograd', 'Russia'),
+	   (5, 'Obelisk of Glory', geography::STGeomFromText('POINT(30.48 24.34)', 4326), 'Tolyatti', 'Russia'),
+	   (6, 'Siegestor', geography::STGeomFromText('POINT(09.08 34.55)', 4326), 'Munich', 'Germany'),
+	   (7, 'Coleseum', geography::STGeomFromText('POINT(53.24 29.32)', 4326), 'Rome', 'Italy');
+
